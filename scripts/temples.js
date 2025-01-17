54cae6d49d7f351def5961531805160f1d621761
Sergio Coria <sergiocoria.my@gmail.com>
2025-01-16 21:25:41 -0400

    W02 Assignment: Picture Album3

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,12 @@ if (lastModifiedElement) {
 } else {
     console.log('Could not find the element with the id "lastModified"');
 }
+
+// Seleccionamos el icono de hamburguesa y el contenedor del menú
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('nav');
+
+// Añadimos un evento de clic para cambiar la clase "active" en el contenedor del menú
+hamburger.addEventListener('click', function () {
+    nav.classList.toggle('active');
+});
