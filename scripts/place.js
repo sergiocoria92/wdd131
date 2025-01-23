c186c215040f87e4a09663312a0ca0e9c750bd3d
Sergio Coria <sergiocoria.my@gmail.com>
2025-01-22 21:11:24 -0400

    2

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+
+const lastModifiedElement = document.querySelector("#lastModified");
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = `Last modified: ${document.lastModified}`;
+} else {
+    console.log('Could not find the element with the id "lastModified"');
+}
