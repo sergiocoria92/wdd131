9b7c5fb8058fdc2fb582ae0986510d2fe8a3987e
Sergio Coria <sergiocoria.my@gmail.com>
2025-01-24 18:19:12 -0400

    9

--- scripts/place.js
+++ scripts/place.js
@@ -8,20 +8,5 @@ if (lastModifiedElement) {
 
 
 
-/*con este codigo se logra cambiar de imagenes segun donde se vea la pagina*/
 
-
-
-window.addEventListener("resize", function() {
-    var image = document.getElementById("meerkatImage");
-
-    if (window.innerWidth <= 767) {
-        image.src = "images/meerkat.jpg";  // Cambiar imagen para móvil
-    } else {
-        image.src = "images/meerkat-large.jpg";   // Cambiar imagen para escritorio
-    }
-});
-
-// Llamamos al evento en carga de página
-window.dispatchEvent(new Event("resize"));
 
